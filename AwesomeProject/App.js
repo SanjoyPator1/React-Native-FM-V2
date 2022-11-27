826ee8009b1a6313fd0a6d0a099b6b3db03ad65f
SanjoyPator <sanjoy18-378@aec.ac.in>
2022-11-27 16:15:06 +0530

    styling

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  safeArea: {flex: 1},
+  containerBgColor: {backgroundColor: 'lavender'},
+  container: {
+    paddingHorizontal: 10,
+    marginVertical: 20,
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: 'red',
+  },
+});
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={[styles.container, styles.containerBgColor]}>
         <Text>Hello</Text>
       </View>
     </SafeAreaView>
